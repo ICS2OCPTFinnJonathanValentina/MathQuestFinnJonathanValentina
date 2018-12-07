f0beff5542011e819d2751ad4e7fee6746e87838
Valentinagm24 <valentina.gaytan-melendez@ocsbstudent.ca>
2018-12-07 13:48:11 -0500

    update

--- splash_screen.lua
+++ splash_screen.lua
@@ -28,7 +28,7 @@ local soundChannel
 ----------------------------------------------------------------------------------------
 -- LOCAL VARIABLES
 -----------------------------------------------------------------------------------------
-local scrollspeed = 7
+local scrollspeed = 9
 -- The local variables for this scene
 local sword = display.newImageRect("Images/CompanyLogoValentina@2x.png", 1100, 800)
 
