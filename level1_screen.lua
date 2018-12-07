0b2b5983b8f0313bc9bda0519e40a05d4d2f7cbc
Jonathan Kene <Q3erty7>
2018-12-07 13:33:41 -0500

    added Math quest

--- level1_screen.lua
+++ level1_screen.lua
@@ -31,7 +31,33 @@ local scene = composer.newScene( sceneName )
 -- The local variables for this scene
 local bkg_image
 
-local
+local character
+local larrow
+local rarrow
+local uarrow
+
+local platform1
+local platform2
+local platform3
+local platform4
+local spikes1
+local spikes2
+
+local heart1
+local heart2
+local heart3
+local numberLives = 4
+
+local motionx = 0
+local SPEED = 7
+local LINEAR_VELOCITY = -100
+local GRAVITY = 7
+
+local questionsAnswered = 0
+
+-----------------------------------------------------------------------------------------
+--  Sound
+----------------------------------------------------------------------------------------- 
 
 
 
@@ -40,9 +66,237 @@ local
 -----------------------------------------------------------------------------------------
 
 
+-- When right arrow is touched, move character right
+local function right (touch)
+    motionx = SPEED
+    character.xScale = 1
+end
+
+-- When left arrow is touched, move character right
+local function left (touch)
+    motionx = -SPEED
+    character.xScale = -1
+end
+
+-- When up arrow is touched, add vertical so it can jump
+local function up (touch)
+    if (character ~= nil) then
+        character:setLinearVelocity( 0, LINEAR_VELOCITY )
+    end
+end
+
+-- Move character horizontally
+local function movePlayer (event)
+    character.x = character.x + motionx
+end
+ 
+-- Stop character movement when no arrow is pushed
+local function stop (event)
+    if (event.phase =="ended") then
+        motionx = 0
+    end
+end
+
+
+local function AddArrowEventListeners()
+    rArrow:addEventListener("touch", right)
+    lArrow:addEventListener("touch", left)
+    uArrow:addEventListener("touch", up)
+end
+
+local function RemoveArrowEventListeners()
+    rArrow:removeEventListener("touch", right)
+    lArrow:removeEventListener("touch", left)
+    uArrow:removeEventListener("touch", up)
+end
+
+local function AddRuntimeListeners()
+    Runtime:addEventListener("enterFrame", movePlayer)
+    Runtime:addEventListener("touch", stop )
+end
+
+local function RemoveRuntimeListeners()
+    Runtime:removeEventListener("enterFrame", movePlayer)
+    Runtime:removeEventListener("touch", stop )
+end
+
+local function ReplaceCharacter()
+    character = display.newImageRect("Images/BoyCharacterValentina.png", 100, 150)
+    character.x = display.contentWidth * 0.5 / 8
+    character.y = display.contentHeight  * 0.1 / 3
+    character.width = 75
+    character.height = 100
+    character.myName = "Questboy"
+
+    -- intialize horizontal movement of character
+    motionx = 0
+
+    -- add physics body
+    physics.addBody( character, "dynamic", { density=0, friction=0.5, bounce=0, rotation=0 } )
+
+
+    -- prevent character from being able to tip over
+    character.isFixedRotation = true
+
+    -- add back arrow listeners
+    AddArrowEventListeners()
+
+    -- add back runtime listeners
+    AddRuntimeListeners()
+end
+
+local function MakeHeartsVisible()
+    heart1.isVisible = true
+    heart2.isVisible = true
+    heart3.isVisible = true
+end
+
+--local function YouLoseTransition()
+--    composer.gotoScene( "you_lose" )
+--end
+
+local function YouWinTransition()
+    composer.gotoScene( "you_Win" )
+end
+
+local function onCollision( self, event )
+    -- for testing purposes
+    --print( event.target )        --the first object in the collision
+    --print( event.other )         --the second object in the collision
+    --print( event.selfElement )   --the element (number) of the first object which was hit in the collision
+    --print( event.otherElement )  --the element (number) of the second object which was hit in the collision
+    --print( event.target.myName .. ": collision began with " .. event.other.myName )
+
+   if ( event.phase == "began" ) then
+
+      if  (event.target.myName == "spikes1") or 
+            (event.target.myName == "spikes2") or
+            (event.target.myName == "spikes3") then
+
+            -- add sound effect here
+
+            -- remove runtime listeners that move the character
+            RemoveArrowEventListeners()
+            RemoveRuntimeListeners()
+
+            -- remove the character from the display
+            display.remove(character)
+
+            -- decrease number of lives
+            numLives = numLives - 1
+
+            if (numLives == 3) then
+                -- update hearts
+                heart1.isVisible = true
+                heart2.isVisible = true
+                heart3.isVisible = true
+                timer.performWithDelay(200, ReplaceCharacter)
+
+           elseif (numLives == 2) then
+                -- update hearts
+                heart1.isVisible = true
+                heart2.isVisible = true
+                heart3.isVisible = false
+                timer.performWithDelay(200, ReplaceCharacter) 
+
+            elseif (numLives == 1) then
+                -- update hearts
+                heart1.isVisible = true
+                heart2.isVisible = false
+                heart3.isVisible = false
+                timer.performWithDelay(200, ReplaceCharacter)
+
+            elseif (numLives == 0) then
+                -- update hearts
+                heart1.isVisible = false
+                heart2.isVisible = false
+                heart3.isVisible = false
+              timer.performWithDelay(200, YouLoseTransition)
+                youLoseSoundChannel = audio.play(youLose)
+            end
+        end
+
+        if  (event.target.myName == "ball1") or
+            (event.target.myName == "ball2") or
+            (event.target.myName == "ball3") then
+
+            -- get the ball that the user hit
+            theBall = event.target
+
+            -- stop the character from moving
+            motionx = 0
+
+            -- make the character invisible
+            character.isVisible = false
+
+            -- show overlay with math question
+            composer.showOverlay( "level1_question", { isModal = true, effect = "fade", time = 100})
+
+            -- Increment questions answered
+            questionsAnswered = questionsAnswered + 1 
+        
+            print("***questions answered = " .. questionsAnswered)
+        end
+
+        if (event.target.myName == "door") then
+            --check to see if the user has answered 5 questions
+            if (questionsAnswered == 3) then
+                Grease_MonkeySoundChannel = audio.play(Grease_Monkey)
+
+                print("***questions answered = " .. questionsAnswered)
+
+                YouWinTransition()
+
+            end
+        end        
+
+    end
+end
 
 
 
+local function AddCollisionListeners()
+    -- if character collides with ball, onCollision will be called
+    spikes1.collision = onCollision
+    spikes1:addEventListener( "collision" )
+    spikes2.collision = onCollision
+    spikes2:addEventListener( "collision" )
+end
+
+local function RemoveCollisionListeners()
+    spikes1:removeEventListener( "collision" )
+    spikes2:removeEventListener( "collision" )
+end
+
+
+local function AddPhysicsBodies()
+    --add to the physics engine
+    physics.addBody( platform1, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+    physics.addBody( platform2, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+    physics.addBody( platform3, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+    physics.addBody( platform4, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+
+    physics.addBody( spikes1, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+    physics.addBody( spikes2, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+
+
+    physics.addBody( spikes1platform, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+    physics.addBody( spikes2platform, "static", { density=1.0, friction=0.3, bounce=0.2 } )
+end
+
+
+local function RemovePhysicsBodies()
+    physics.removeBody(platform1)
+    physics.removeBody(platform2)
+    physics.removeBody(platform3)
+    physics.removeBody(platform4)
+
+    physics.removeBody(spikes1)
+    physics.removeBody(spikes2)
+
+    physics.removeBody(spikes1platform)
+    physics.removeBody(spikes2platform)
+end
 
 
 
