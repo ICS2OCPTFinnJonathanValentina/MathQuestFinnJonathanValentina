4558e64b205a73097b6426b6ddd9d63d9b4ebc63
Valentinagm24 <valentina.gaytan-melendez@ocsbstudent.ca>
2018-12-11 14:17:20 -0500

    updatye

--- you_win.lua
+++ you_win.lua
@@ -44,7 +44,7 @@ function scene:create( event )
     local sceneGroup = self.view
 
     -- Display background
-    bkg = display.newImage("Images/YouWinScreenJonathan.png")
+    bkg = display.newImage("Images/YouWinScreenValentina@2x.png")
     bkg.x = display.contentCenterX
     bkg.y = display.contentCenterY
     bkg.width = display.contentWidth
