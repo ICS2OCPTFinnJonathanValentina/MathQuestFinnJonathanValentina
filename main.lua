ba0a63a37e80cf2527ecfd11c8c433e177d8c7f7
Valentinagm24 <valentina.gaytan-melendez@ocsbstudent.ca>
2019-01-11 13:38:19 -0500

    updated you win

--- main.lua
+++ main.lua
@@ -16,7 +16,7 @@ local composer = require( "composer" )
 -----------------------------------------------------------------------------------------
 
 -- Go to the intro screen
-composer.gotoScene( "level1_screen" )
+composer.gotoScene( "level3_screen" )
 
 
 
