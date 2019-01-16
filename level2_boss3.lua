dd92df6e123159c9bc400eb5de75bf10bf9f2598
FinnLeduc <43419123+FinnLeduc@users.noreply.github.com>
2019-01-16 14:19:44 -0500

    dd

--- level2_boss3.lua
+++ level2_boss3.lua
@@ -84,7 +84,7 @@ end
 
 local function YouLose()
     if (numLives == 0) then
-        YouLoseTransition()
+        NextLevelTransition()
     else
         timer.performWithDelay(1000, NextLevelTransition)
     end
@@ -113,7 +113,7 @@ local function TouchListenerWrongAnswer(touch)
         numLives = numLives - 1
         print ("***numLives: TouchListenerWrongAnswer1 = " .. numLives)
         
-        timer.performWithDelay(1000, NextLevelTransition1) 
+        timer.performWithDelay(1000, YouLose) 
     end 
 
 end
