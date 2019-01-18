f48171b85c70dea3c80606773b065915cf8e89fd
LynnetteR <Lynnette.Raffin@ocsb.ca>
2019-01-18 10:00:56 -0500

    update main

--- main.lua
+++ main.lua
@@ -15,14 +15,8 @@ local composer = require( "composer" )
 
 -----------------------------------------------------------------------------------------
 
-
 composer.gotoScene("splash_screen" )
 
-
-<<<<<<< HEAD
-composer.gotoScene("splash_screen" )
-=======
->>>>>>> 032cdc16cd328c3e073ea213133a87c1e2c838f6
 
 
 
